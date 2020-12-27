dd46d402ce5c904df342faf0d48112d98b2da17b
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-27 16:18:41 +0100

    Add about func

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, '',{})
+    return render(request, 'index.html',{})
+
+def about(request):
+    return render(request, 'about.html',{})
