e36a3220c72b4a5d442ab3f3e8afdcf0967f2e54
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-28 12:59:54 +0100

    Add posgres settings

--- src/src/settings.py
+++ src/src/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER':'postgres',
+        'PASSWORD': 'bartosz15',
+        'HOST': 'localhost'
     }
 }
 
