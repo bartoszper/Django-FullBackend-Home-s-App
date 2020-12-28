795f02a35c9cc25392dd6ff652c916d34c0c8a9f
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-28 18:03:30 +0100

    Add ListingAdmin

--- src/listings/admin.py
+++ src/listings/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Listing
 
-# Register your models here.
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'realtor','list_date')
+    list_display_links = ('id','title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title','description','state','city','price')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
