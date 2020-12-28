364a3059a67240d17c7c5981e9ff8a730e3816e0
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-28 21:22:02 +0100

    Update pagination

--- src/listings/views.py
+++ src/listings/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
     listings = Listing.objects.all()
+    #Pagination
+    paginator = Paginator(listings, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'listings':listings
+        'listings': page_obj
     }
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
