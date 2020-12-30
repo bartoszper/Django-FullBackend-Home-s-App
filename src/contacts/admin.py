cd9094f68d93cf05d994d686267ebcbd5e63d4ba
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-30 22:18:38 +0100

    Add ContactAdmin model

--- src/contacts/admin.py
+++ src/contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','name','phone','email','listing')
+    list_display_links = ('id','name')
+    list_per_page = 25
+    list_filter = ('listing',)
+    search_fields = ('listing','name','email')
+
+
+admin.site.register(Contact, ContactAdmin)
