92c2f8fab882125be1584a7bbe9fa13959b18caa
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-30 17:18:13 +0100

    Update views.py

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def register(request):
+    if request.method == 'POST':
+    
+    #Register users
+    
+        return redirect('register')
     return render(request, 'accounts/register.html')
 
 
 def login(request):
+    if request.method == 'POST':
+        return redirect('login')
     return render(request, 'accounts/login.html')
 
 
